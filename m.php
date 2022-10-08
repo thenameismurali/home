c408b73ef52b59b500aaf14c6e200a00cd554250
thenameismurali <115346776+thenameismurali@users.noreply.github.com>
2022-10-08 19:40:15 +0530

    Add files via upload

---
+++ m.php
@@ -0,0 +1,46 @@
+ <?php
+ <html style="background-color:white">   
+<ul> 
+    <li type="none">
+<b style="color:aqua; font-size:50px; padding-left:0px;padding-right:20px;">Afri&nbsp;</b><b style="color:pink; font-size:50px;padding-right:20px;">Scuti</b></li>
+<li type="none" >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;proudly presents <b>Peter Obi & Asiwaju Tinubu</b> </li> 
+</ul>
+<img align="right" width="500px" height="250px" src="/home/rgukt/Downloads/bd865d5a-bced-49a8-91e1-3a5e16e21a65.jpeg"><a style="text-decoration:none;"href="https://flutterwave.com/pay/beobidient"><h1 align="right" style="align-items:center; padding:90px;padding-right:540px;padding-left:10px;" ><button>Order</button></h1></a>
+
+</div> 
+ <style>
+img{ 
+    border:5px ; 
+    
+}   
+button{ 
+    width:150px;  
+    font-size: 40px; 
+    padding-right:80px; 
+    background-color:yellow;
+}
+
+
+</style> 
+
+<br><br><br> 
+<br> 
+<br><br><br> 
+<br> 
+<br><br><br> 
+<br> 
+
+<br><br><br> 
+<br> 
+<br><br><br> <br><br><br> 
+<br> 
+  
+ 
+<br> 
+<footer>
+
+<h1><b style="color:aqua;">Afri</b>&nbsp;&nbsp;&nbsp;<b style="color:pink;">Scuti</b></h1><br>
+ 
+</footer>
+</html> 
+?>
